959dfb5abc48f7191195748bbe4077d5389e630d
Vegi Sai Kiran <saikiransonuvegi@gmail.com>
2022-01-13 01:31:43 +0530

    reverse

--- src/com/company/reverse.java
+++ src/com/company/reverse.java
@@ -5,13 +5,12 @@ import java.util.Scanner;
 public class reverse {
     public static void main(String [] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Input a string: ");
-        char[] letters = scanner.nextLine().toCharArray();
-        System.out.print("Reverse string: ");
-        for (int i = letters.length - 1; i >= 0; i--) {
-            System.out.print(letters[i]);
+        System.out.print("Enter string : ");
+        char[] letter = scanner.nextLine().toCharArray();
+        System.out.print("Reverse string : ");
+        for (int i = letter.length-1;i>=0;i--){
+            System.out.print(letter[i]);
         }
-        System.out.print("\n");
 
     }
 }
