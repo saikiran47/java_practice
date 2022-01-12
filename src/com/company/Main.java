39af68e238345eb80431437655071c70fd7eb1c7
Vegi Sai Kiran <saikiransonuvegi@gmail.com>
2022-01-12 12:52:43 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("finding area and perimeter of circle");
+        double r = 7.5;
+        double p = 2*Math.PI*r;
+        double a = Math.PI*r*r;
+        System.out.println("Perimeter is : "+p);
+        System.out.println("Area  is : "+a);
+
+
+
+
+
+    }
+
+}
+
